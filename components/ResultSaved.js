2f128b84a4971726bfc8daef84631267923fd30f
jinyu <jwu427@wisc.edu>
2020-04-20 17:44:21 -0500

    added unfavorite

--- components/ResultSaved.js
+++ components/ResultSaved.js
@@ -7,9 +7,11 @@ import {
   Title,
   Paragraph,
   List,
+  IconButton,
 } from 'react-native-paper';
 import { View, StyleSheet, Text, ScrollView, FlatList } from 'react-native';
 import Comment from './CommentItem.js';
+import { NativeModules } from "react-native";
 
 class ResultSaved extends React.Component {
   constructor(props) {
@@ -30,15 +32,53 @@ class ResultSaved extends React.Component {
     });
   }
 
+  toUnSaved() {
+    const { url, token } = this.props;
+    fetch(url + '/v1/terms/unfavorite', {
+      method: 'PUT',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: '*/*',
+        token: token,
+      },
+      body: JSON.stringify({
+        id: this.props.course_id,
+      })
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(result => {
+        if (result.success === true) {
+          const terms = result.data;
+          this.props.updateList();
+          alert("Course is Deleted");
+        } else {
+          alert("You have already deleted this course to your favorite list.");
+        }
+      })
+      .catch(err => {
+        alert(err);
+      });
+  }
+
   render() {
     return (
-
       <ScrollView>
         <Card style={styles.card}>
           <Card.Content>
             <Title>{this.props.title}</Title>
 
             <Paragraph>{this.props.description}</Paragraph>
+            <View style={{ position: "absolute", right: 3, top: 0 }}>
+              <IconButton
+                icon="heart-off"
+                color="#FF1493"
+                size={30}
+                onPress={() => this.toUnSaved()}
+              />
+            </View>
           </Card.Content>
           <Card.Actions>
             <Button onPress={() => this.toComment()}>Detail</Button>
--- components/screen/SavedScreen.js
+++ components/screen/SavedScreen.js
@@ -16,11 +16,37 @@ class SavedScreen extends React.Component {
       visible: true,
       token: '',
     };
+    this.updateList = this.updateList.bind(this);
   }
 
   static navigationOptions = {
     title: 'Saved Courses',
   };
+
+  updateList() {
+    const { url, email, token } = this.props.navigation.state.params;
+    fetch(url + '/v1/users/email/' + email, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: '*/*'
+      },
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(result => {
+        if (result.success === true) {
+          // let terms = result.data;
+          let terms = result.data.favorite;
+          this.setState({ terms: terms, visible: false, token: token });
+        }
+      })
+      .catch(err => {
+        alert(err);
+      });
+  }
 
   componentDidMount() {
     const { url, email, token } = this.props.navigation.state.params;
@@ -67,6 +93,7 @@ class SavedScreen extends React.Component {
               url={this.state.url}
               email={this.state.email}
               id={this.state.id}
+              updateList={this.updateList}
             />
           )}
           keyExtractor={item => item.id}
